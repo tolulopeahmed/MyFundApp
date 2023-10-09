489647615edfd8a4e78a857d6653b4578ee1134b
Tolulope Ahmed <81494716+tolulopeahmed@users.noreply.github.com>
2023-10-09 10:11:31 +0100

    Withdraw to Bank working perfectly

--- myfundproject/settings.py
+++ myfundproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-rct_mdzr=x!99kwy+xy1$#x=5_+!_-dynu%z&!jx_-qkj7*%*%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.86.34','192.168.226.34','localhost', '192.168.226.34', '192.168.176.34', '10.10.4.174', '192.168.84.34', '10.10.4.174' ]
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.3.34','192.168.226.34','localhost', '192.168.226.34', '192.168.176.34', '10.10.4.174', '192.168.84.34', '10.10.4.174' ]
 
 
 # Application definition
