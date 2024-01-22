6de083c6aa17fa81e97f30db35634cc1073003c1
Bambotims <timilehinadebambo@gmail.com>
2024-01-23 00:08:54 +0100

    images fix

--- myfundproject/urls.py
+++ myfundproject/urls.py
@@ -9,22 +9,31 @@ from django.views.decorators.csrf import csrf_exempt
 from authentication.views import CustomGraphQLView
 from authentication.schema import schema  # Adjust the import path
 from graphql_jwt.decorators import jwt_cookie
+from django.views.static import serve
 
 
 def welcome(request):
-    template = loader.get_template('welcome.html')
+    template = loader.get_template("welcome.html")
     return HttpResponse(template.render({}, request))
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/', include('authentication.urls')),  
-    path('', welcome, name='welcome'), 
-    path('graphql/', csrf_exempt(jwt_cookie(CustomGraphQLView.as_view(graphiql=True, schema=schema)))),
-
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/", include("authentication.urls")),
+    path("", welcome, name="welcome"),
+    path(
+        "graphql/",
+        csrf_exempt(
+            jwt_cookie(CustomGraphQLView.as_view(graphiql=True, schema=schema))
+        ),
+    ),
+    path(
+        "media/<path:path>/",
+        serve,
+        {"document_root": settings.MEDIA_ROOT, "show_indexes": True},
+    ),
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
